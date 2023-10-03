26b0adac99d016ac0cd41c4ee9884d1e5cf3c7a7
Ryan Valera <valeraryan@gmail.com>
2023-10-03 16:10:29 -0700

    Created a <FormControl> style component and nested the conditionals from the <FormControl> tag to the style component styled.div code-block.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,9 +1,47 @@
 import React, { useState } from 'react';
-
+import styled from "styled-components";
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+// import './CourseInput.css';
+
+const FormControl = styled.div`
+    
+      margin: 0.5rem 0;
+
+    & label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: 0.5rem;
+      color: ${props => (props.valid ? 'red' : 'black')};
+    }
+
+    & input {
+      display: block;
+      width: 100%;
+      border: 1px solid ${props => (props.valid ? 'red' : '#ccc')};
+      background: ${props => (props.valid ? 'red' : 'transparent')};
+      font: inherit;
+      line-height: 1.5rem;
+      padding: 0 0.25rem;
+    }
+
+    & input:focus {
+      outline: none;
+      background: #fad0ec;
+      border-color: #8b005d;
+    }
+
+    //&.invalid input {
+    //  border-color: red;
+    //  background-color: red;
+    //}
+    //
+    //&.invalid label {
+    //  color: red;
+    //}
+  `;
 
 const CourseInput = props => {
+
   const [enteredValue, setEnteredValue] = useState('');
   // Ex.1 Create state (boolean) to track if form is valid
   const [isValid, setIsValid] = useState(true);
@@ -28,11 +66,11 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl valid={!isValid}>
         {/*Ex.1 Create a red WARNING conditional inline-style using isValid state */}
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
