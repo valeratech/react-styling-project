e06babf5b2bf82b3eb83a8e9d619d231f4f05611
Ryan Valera <valeraryan@gmail.com>
2023-10-02 20:41:53 -0700

    Modified CourseInput.js module to include a an isValid useState hook to track if the Course Goal form is valid. Based on the state using conditional blocks, inline conditional styles were used to modify the form (styles: color/border/background) using the color red and reset when conditions were met (isValid).

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,33 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  // Ex.1 Create state (boolean) to track if form is valid
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    // Ex.1 Reset red WARNING conditional inline-style and isValid state when user begins typing
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // Ex.1 Add input validation to the Course Goal Input Form if no input is present when trying to submit
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        {/*Ex.1 Create a red WARNING conditional inline-style using isValid state */}
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{borderColor: !isValid ? 'red' : 'black', backgroundColor: !isValid ? 'red' : 'transparent'}} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
