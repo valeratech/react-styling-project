a4d211316348015c4488b8d440df5d76633577c9
Ryan Valera <valeraryan@gmail.com>
2023-10-04 11:18:43 -0700

    Replaced styled-components with CSS Modules to handle the validation and styling for both the Button.js and CourseInput.js modules. Renamed their associated style sheets to conform with CSS Modules.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,38 +1,39 @@
 import React, { useState } from 'react';
-import styled from "styled-components";
+// Ex.4 import styled from "styled-components";
 import Button from '../../UI/Button/Button';
-// import './CourseInput.css';
+// Ex. 5 Use correct syntax to import CSS Modules file
+import styles from './CourseInput.module.css';
 
 // Ex.2 Replace <Div> component that references a 'form-control' class with a styled-component
-const FormControl = styled.div`
-    
-      margin: 0.5rem 0;
-
-    & label {
-      font-weight: bold;
-      display: block;
-      margin-bottom: 0.5rem;
-      //Ex.3 Reference the prop from the <FormControl> using an arrow function to show the validation styling
-      color: ${props => props.invalid ? 'red' : 'black'};
-    }
-
-    & input {
-      display: block;
-      width: 100%;
-      //Ex.3 Reference the prop from the <FormControl> using an arrow function to show the validation styling 
-      border: 1px solid ${props => props.invalid ? 'red' : 'black'};
-      background-color: ${props => props.invalid ? 'pink' : 'transparent'};
-      font: inherit;
-      line-height: 1.5rem;
-      padding: 0 0.25rem;
-    }
-
-    & input:focus {
-      outline: none;
-      background: #fad0ec;
-      border-color: #8b005d;
-    }
-  `;
+// const FormControl = styled.div`
+//
+//       margin: 0.5rem 0;
+//
+//     & label {
+//       font-weight: bold;
+//       display: block;
+//       margin-bottom: 0.5rem;
+//       //Ex.3 Reference the prop from the <FormControl> using an arrow function to show the validation styling
+//       color: ${props => props.invalid ? 'red' : 'black'};
+//     }
+//
+//     & input {
+//       display: block;
+//       width: 100%;
+//       //Ex.3 Reference the prop from the <FormControl> using an arrow function to show the validation styling
+//       border: 1px solid ${props => props.invalid ? 'red' : 'black'};
+//       background-color: ${props => props.invalid ? 'pink' : 'transparent'};
+//       font: inherit;
+//       line-height: 1.5rem;
+//       padding: 0 0.25rem;
+//     }
+//
+//     & input:focus {
+//       outline: none;
+//       background: #fad0ec;
+//       border-color: #8b005d;
+//     }
+//   `;
 
 const CourseInput = props => {
 
@@ -61,10 +62,11 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/*Ex.2 Create a prop for FormControl  referenced in the styled-component block at the top of the module */}
-      <FormControl invalid={!isValid}>
+      {/*Ex.5 Call the styles.button property from the import to create the styling*/}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,41 +1,44 @@
 import React from 'react';
-import styled from "styled-components";
-// import './Button.css';
+// Ex.4 import styled from "styled-components";
 
-// Ex.2 Replace <Button> component with a styled-component
-const Button = styled.button`
-    // Ex. 4 Set the width for mobile-devices based on the media-query configured below
-    width: 100%;
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
-    // Create a Styled-Component media query to dyanmically change based on the type of device
-    @media (min-width: 768px) {
-      width: auto;
-  }
+// Ex. 5 Use correct syntax to import CSS Modules file
+import styles from './Button.module.css';
 
-  &:focus {
-    outline: none;
-  }
+// // Ex.2 Replace <Button> component with a styled-component
+// const Button = styled.button`
+//     // Ex. 4 Set the width for mobile-devices based on the media-query configured below
+//     width: 100%;
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #8b005d;
+//     color: white;
+//     background: #8b005d;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//     cursor: pointer;
+//     // Create a Styled-Component media query to dynamically change based on the type of device
+//     @media (min-width: 768px) {
+//       width: auto;
+//   }
+//
+//   &:focus {
+//     outline: none;
+//   }
+//
+//   &:hover,
+//   &active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
-  &:hover,
-  &active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+      // Ex.5 Call the styles.button property from the import to create the styling
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
